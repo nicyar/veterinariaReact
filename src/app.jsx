ebac151cd7522364e82fc148a61734de00dd5c04
nico <nayarnicolas2@hotmail.com>
2022-07-25 16:11:59 -0300

    localStorage

--- src/app.jsx
+++ src/app.jsx
@@ -8,9 +8,17 @@ function App() {
   
   const[pacientes,setPacientes]=useState([]);
   const[paciente,setPaciente]=useState({});
+  
+  useEffect(()=>{
+    const obtenerLS=()=>{
+      const pacientesLS=JSON.parse(localStorage.getItem('pacientes')) ?? [] ;
+      setPacientes(pacientesLS)
+    }
+    obtenerLS()
+  },[])
 
   useEffect(()=>{
-    console.log('componente listo o cambio de paciente')
+    localStorage.setItem('pacientes', JSON.stringify(pacientes))
   },[pacientes])
 
   const elimminarPaciente=(id)=>{
